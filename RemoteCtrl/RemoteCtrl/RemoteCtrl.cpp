90ecd10c1134c22a57d4496890b3e5a13f432771
red-fox <591527341@qq.com>
2024-01-26 03:03:54 +0800

    隐藏控制命令界面，后台运行 完成包设计和实现

--- RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
+++ RemoteCtrl/RemoteCtrl/RemoteCtrl.cpp
@@ -9,7 +9,11 @@
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
-
+//#pragma comment( linker,"/subsystem:windows /entry:WinMainCRTStartup")
+//#pragma comment( linker,"/subsystem:windows /entry:mainCRTStartup")
+//#pragma comment( linker,"/subsystem:console /entry:mainCRTStartup")
+//#pragma comment( linker,"/subsystem:console /entry:WinMainCRTStartup")
+//后台运行设置
 
 // 唯一的应用程序对象
 
@@ -19,49 +23,49 @@ using namespace std;
 
 int main()
 {
-    int nRetCode = 0;
+	int nRetCode = 0;
 
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+	HMODULE hModule = ::GetModuleHandle(nullptr);
 
-    if (hModule != nullptr)
-    {
-        // 初始化 MFC 并在失败时显示错误       
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
-            wprintf(L"错误: MFC 初始化失败\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            // TODO: 在此处为应用程序的行为编写代码。
-            CServerSocket* pserver = CServerSocket::getInstance();
-            int count = 0;
-            if (pserver->InitSocket() == false) {
-                MessageBox(NULL, _T("网络初始化异常，未能成功初始hi，请检查网络设置！"), _T("网络初始化错误！"), MB_OK | MB_ICONERROR);
-                exit(0);
-            }
-            while (CServerSocket::getInstance() != NULL) {
-                if (pserver->AcceptClient() == false) {
-                    if (count >= 3) {
-                        MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-                        exit(0);
-                    }
-                    MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
-                    count++;
-                }
-                int ret = pserver->DealCommand();
-                //TODO:
-            }
+	if (hModule != nullptr)
+	{
+		// 初始化 MFC 并在失败时显示错误       
+		if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
+		{
+			// TODO: 在此处为应用程序的行为编写代码。
+			wprintf(L"错误: MFC 初始化失败\n");
+			nRetCode = 1;
+		}
+		else
+		{
+			// TODO: 在此处为应用程序的行为编写代码。
+			CServerSocket* pserver = CServerSocket::getInstance();
+			int count = 0;
+			if (pserver->InitSocket() == false) {
+				MessageBox(NULL, _T("网络初始化异常，未能成功初始hi，请检查网络设置！"), _T("网络初始化错误！"), MB_OK | MB_ICONERROR);
+				exit(0);
+			}
+			while (CServerSocket::getInstance() != NULL) {
+				if (pserver->AcceptClient() == false) {
+					if (count >= 3) {
+						MessageBox(NULL, _T("多次无法正常接入用户，结束程序！"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+						exit(0);
+					}
+					MessageBox(NULL, _T("无法正常接入用户，自动重试"), _T("接入用户失败！"), MB_OK | MB_ICONERROR);
+					count++;
+				}
+				int ret = pserver->DealCommand();
+				//TODO:
+			}
 
-        }
-    }
-    else
-    {
-        // TODO: 更改错误代码以符合需要
-        wprintf(L"错误: GetModuleHandle 失败\n");
-        nRetCode = 1;
-    }
+		}
+	}
+	else
+	{
+		// TODO: 更改错误代码以符合需要
+		wprintf(L"错误: GetModuleHandle 失败\n");
+		nRetCode = 1;
+	}
 
-    return nRetCode;
+	return nRetCode;
 }
