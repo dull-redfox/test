ccb8aca3649448f6547b262c792700893b7f8173
red-fox <591527341@qq.com>
2024-04-12 11:40:06 +0800

    1 解决显示命令和鼠标命令冲突导致程序卡死问题 2解决显示内容变化过大，导致的接收程序卡死的问题 备注 1 鼠标坐标转换问题 2 文件下载未测试

--- RemoteCtrl/RemoteClient/ClientSocket.cpp
+++ RemoteCtrl/RemoteClient/ClientSocket.cpp
@@ -34,22 +34,47 @@ void Dump(BYTE* pData, size_t nSize)
 	OutputDebugStringA(strOut.c_str());
 }
 
+bool CClientSocket::InitSocket()
+{
+	if (m_sock != INVALID_SOCKET)CloseSocket();
+	m_sock = socket(PF_INET, SOCK_STREAM, 0);
+	if (m_sock == -1)return false;
+	sockaddr_in serv_adr;
+	memset(&serv_adr, 0, sizeof(serv_adr));
+	serv_adr.sin_family = AF_INET;
+	serv_adr.sin_addr.s_addr = htonl(m_nIP);
+	serv_adr.sin_port = htons(m_nPort);
+	if (serv_adr.sin_addr.s_addr == INADDR_NONE) {
+		AfxMessageBox("指定的IP地址，不存在");
+		return false;
+	}
+	int ret = connect(m_sock, (sockaddr*)&serv_adr, sizeof(serv_adr));
+	if (ret == -1) {
+		AfxMessageBox("连接失败");
+		TRACE("连接失败,%d %s\r\n", WSAGetLastError(), GetErrorInfo(WSAGetLastError()).c_str());
+		return false;
+	}
+	return true;
+}
+
 bool CClientSocket::SendPacket(const CPacket& pack, std::list<CPacket>& lstPacks, bool isAutoClosed)
 {
-	if (m_sock == INVALID_SOCKET) {
+	if (m_sock == INVALID_SOCKET&&m_hThread==INVALID_HANDLE_VALUE) {
 		//if (InitSocket() == false)return false;
-		_beginthread(&CClientSocket::threadEntry, 0, this);
+		m_hThread=(HANDLE)_beginthread(&CClientSocket::threadEntry, 0, this);
 	}
+	m_lock.lock();
 	auto pr = m_mapAck.insert(std::pair<HANDLE, std::list<CPacket>>(pack.hEvent, lstPacks));
 	m_mapAutoClosed.insert(std::pair<HANDLE, bool>(pack.hEvent,isAutoClosed));
-
 	m_lstSend.push_back(pack);
+	m_lock.unlock();
 	WaitForSingleObject(pack.hEvent, INFINITE);
 	std::map<HANDLE, std::list<CPacket>>::iterator it;
 	it = m_mapAck.find(pack.hEvent);
 	if (it != m_mapAck.end()) {
-
+		m_lock.lock();
 		m_mapAck.erase(it);
+		m_lock.unlock();
 		return true;
 	}
 	return false;
@@ -71,7 +96,9 @@ void CClientSocket::threadFunc()
 	while (m_sock != INVALID_SOCKET) {
 		if (m_lstSend.size() > 0) {
 			TRACE("lstSend size:%d\r\n", m_lstSend.size());
+			m_lock.lock();
 			CPacket& head = m_lstSend.front();
+			m_lock.unlock();
 			if (Send(head) == false) {
 				TRACE("发送失败！\r\n");
 				continue;
@@ -95,6 +122,7 @@ void CClientSocket::threadFunc()
 							index -= size;
 							if (it0->second) {
 								SetEvent(head.hEvent);
+								break;
 							}
 						}
 					}
@@ -106,13 +134,14 @@ void CClientSocket::threadFunc()
 					}
 				} while (it0->second == false);
 			}
-			
+			m_lock.lock();
 			m_lstSend.pop_front();
+			m_lock.unlock();
 			if (InitSocket() == false) {
 				InitSocket();
 			}
-			InitSocket();
 		}
+		Sleep(1);
 	}
 	CloseSocket();
 }
